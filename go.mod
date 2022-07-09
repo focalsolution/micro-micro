2ccc91a5b349c824d91cd1c07843840eb24714ab
Rachapong Pornwiriyangkura <rachapong@focalsolution.com>
2022-07-09 18:44:41 +0700

    Init

---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/micro/micro
+
+go 1.13
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+	github.com/cloudflare/cloudflare-go v0.10.6
+	github.com/eknkc/basex v1.0.0 // indirect
+	github.com/go-acme/lego/v3 v3.1.0
+	github.com/golang/protobuf v1.3.2
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.3
+	github.com/hako/branca v0.0.0-20180808000428-10b799466ada
+	github.com/micro/cli v0.2.0
+	github.com/micro/go-micro v1.16.0
+	github.com/miekg/dns v1.1.22
+	github.com/olekukonko/tablewriter v0.0.1
+	github.com/patrickmn/go-cache v2.1.0+incompatible
+	github.com/pborman/uuid v1.2.0
+	github.com/pkg/errors v0.8.1
+	github.com/pquerna/otp v1.2.0
+	github.com/serenize/snaker v0.0.0-20171204205717-a683aaf2d516
+	github.com/xlab/treeprint v0.0.0-20181112141820-a009c3971eca
+	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	google.golang.org/grpc v1.24.0
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+	gopkg.in/olivere/elastic.v5 v5.0.82
+)
---
+++ service/service.go
@@ -0,0 +1,130 @@
+// Package service provides a micro service
+package service
+
+import (
+	"os"
+	"strings"
+
+	"github.com/micro/cli"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/proxy"
+	"github.com/micro/go-micro/proxy/grpc"
+	"github.com/micro/go-micro/proxy/http"
+	"github.com/micro/go-micro/proxy/mucp"
+	"github.com/micro/go-micro/runtime"
+	"github.com/micro/go-micro/server"
+	"github.com/micro/go-micro/util/log"
+	"github.com/micro/go-micro/util/mux"
+)
+
+func run(ctx *cli.Context, opts ...micro.Option) {
+	log.Name("service")
+
+	name := ctx.String("name")
+	address := ctx.String("address")
+	endpoint := ctx.String("endpoint")
+
+	if len(name) > 0 {
+		opts = append(opts, micro.Name(name))
+	}
+
+	if len(address) > 0 {
+		opts = append(opts, micro.Address(address))
+	}
+
+	if len(endpoint) == 0 {
+		endpoint = proxy.DefaultEndpoint
+	}
+
+	var p proxy.Proxy
+
+	switch {
+	case strings.HasPrefix(endpoint, "grpc"):
+		p = grpc.NewProxy(proxy.WithEndpoint(endpoint))
+	case strings.HasPrefix(endpoint, "http"):
+		p = http.NewProxy(proxy.WithEndpoint(endpoint))
+	default:
+		p = mucp.NewProxy(proxy.WithEndpoint(endpoint))
+	}
+
+	// run the service if asked to
+	if len(ctx.Args()) > 0 {
+		args := []runtime.CreateOption{
+			runtime.WithCommand(ctx.Args()[0], ctx.Args()[1:]...),
+			runtime.WithOutput(os.Stdout),
+		}
+
+		r := runtime.NewRuntime()
+
+		// register the service
+		r.Create(&runtime.Service{
+			Name: name,
+		}, args...)
+
+		// start the runtime
+		r.Start()
+
+		// stop the runtime
+		defer func() {
+			r.Delete(&runtime.Service{
+				Name: name,
+			})
+			r.Stop()
+		}()
+	}
+
+	log.Logf("Service [%s] Serving %s at endpoint %s\n", p.String(), name, endpoint)
+
+	// new service
+	service := micro.NewService(opts...)
+
+	// create new muxer
+	muxer := mux.New(name, p)
+
+	// set the router
+	service.Server().Init(
+		server.WithRouter(muxer),
+	)
+
+	// run service
+	service.Run()
+}
+
+func Commands(options ...micro.Option) []cli.Command {
+	command := cli.Command{
+		Name:  "service",
+		Usage: "Run a micro service",
+		Action: func(ctx *cli.Context) {
+			run(ctx, options...)
+		},
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "name",
+				Usage:  "Name of the service",
+				EnvVar: "MICRO_SERVICE_NAME",
+			},
+			cli.StringFlag{
+				Name:   "address",
+				Usage:  "Address of the service",
+				EnvVar: "MICRO_SERVICE_ADDRESS",
+			},
+			cli.StringFlag{
+				Name:   "endpoint",
+				Usage:  "The local service endpoint. Defaults to localhost:9090",
+				EnvVar: "MICRO_SERVICE_ENDPOINT",
+			},
+		},
+	}
+
+	for _, p := range Plugins() {
+		if cmds := p.Commands(); len(cmds) > 0 {
+			command.Subcommands = append(command.Subcommands, cmds...)
+		}
+
+		if flags := p.Flags(); len(flags) > 0 {
+			command.Flags = append(command.Flags, flags...)
+		}
+	}
+
+	return []cli.Command{command}
+}
